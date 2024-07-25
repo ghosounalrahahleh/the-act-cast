efb648eed1d13d484056bd643d11c261090f9e1d
Ghosoon Alrahahleh <ghosoun.alrahahleh@gmail.com>
2024-07-25 21:06:24 +0300

    add: public path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/the-act-cast/"
+  base: "/the-act-cast/",
+  define: {
+    'process.env.REACT_APP_PUBLIC_PATH': JSON.stringify(process.env.REACT_APP_PUBLIC_PATH),
+  },
 })
